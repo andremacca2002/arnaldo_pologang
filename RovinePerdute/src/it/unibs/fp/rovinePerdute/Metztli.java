7a5ffe5e5b510ca999081074e1892e303e59dc5f
andremacca2002 <104765382+andremacca2002@users.noreply.github.com>
2022-06-14 16:30:55 +0200

    vecchie rovine tolte

--- RovinePerdute/src/it/unibs/fp/rovinePerdute/Metztli.java
+++
@@ -1,9 +0,0 @@
-package it.unibs.fp.rovinePerdute;
-
-public class Metztli extends Percorso {	
-	public double calcolaDistanza(Citta citta1, Citta citta2) {
-		double distanza=Math.abs(citta1.getH()-citta2.getH());
-		return distanza;
-	}			
-}
-
